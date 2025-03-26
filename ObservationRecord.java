e344c74ccb520f62b2e275e8dee0b0ed39a46866
klukovki23 <klukovki23@student.oulu.fi>
2025-03-26 13:22:15 +0200

    commit1

--- ObservationRecord.java
+++ ObservationRecord.java
@@ -1,31 +1,43 @@
-package com.viikko4;
+package com.viikko5;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-
+import java.util.List;
 
 public class ObservationRecord {
     private String recordIdentifier;
     private String recordDescription;
     private String recordPayload;
     private String recordRightAscension;
-    private String recordDeclination;
+    private String recordDeclination; 
     private String recordTimeReceived;
+    private String recordOwner;
+    private List<Observatory> observatory;
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
 
+    
     public ObservationRecord(String recordIdentifier, String recordDescription, 
                              String recordPayload, String recordRightAscension, 
-                             String recordDeclination) {
+                             String recordDeclination, String recordOwner, List<Observatory> observatory) {
         this.recordIdentifier = recordIdentifier;
         this.recordDescription = recordDescription;
         this.recordPayload = recordPayload;
         this.recordRightAscension = recordRightAscension;
-        this.recordDeclination = recordDeclination;
+        this.recordDeclination = recordDeclination; 
+        this.recordOwner = recordOwner;
+        this.observatory = observatory;
 
-         this.recordTimeReceived = ZonedDateTime.now(java.time.ZoneId.of("UTC")).format(formatter);
-    
+        this.recordTimeReceived = ZonedDateTime.now(java.time.ZoneId.of("UTC")).format(formatter);
     }
+
+    
+
+    public String getRecordOwner() { return recordOwner; }
+    public void setRecordOwner(String recordOwner) { this.recordOwner = recordOwner; }
+
+    public List<Observatory> getObservatory() { return observatory; }
+    public void setObservatories(List<Observatory> observatory) { this.observatory = observatory; }
 
     public String getRecordIdentifier() { return recordIdentifier; }
     public void setRecordIdentifier(String recordIdentifier) { this.recordIdentifier = recordIdentifier; }
@@ -44,17 +56,53 @@ public class ObservationRecord {
 
     public String getRecordTimeReceived() { return recordTimeReceived; }
     public void setRecordTimeReceived(String recordTimeReceived) { this.recordTimeReceived = recordTimeReceived; }
-    
-    @Override
+
+  
+        
+        @Override
     public String toString() {
-        return "ObservationRecord{" +
-                "recordIdentifier='" + recordIdentifier + '\'' +
+        return  "recordIdentifier='" + recordIdentifier + '\'' +
                 ", recordDescription='" + recordDescription + '\'' +
                 ", recordPayload='" + recordPayload + '\'' +
                 ", recordRightAscension='" + recordRightAscension + '\'' +
                 ", recordDeclination='" + recordDeclination + '\'' +
                 ", recordTimeReceived='" + recordTimeReceived + '\'' +
+                ", recordOwner='" + recordOwner + '\'' +
+                ", observatory=" + (observatory != null ? observatory.toString() : "[]") +
                 '}';
     }
-}
+        
+    
+        
+    
+    public static class Observatory {
 
+        private String observatoryName;
+        private double latitude;
+        private double  longitude;
+
+        public Observatory(String observatoryName, double latitude, double longitude) {
+            this.observatoryName = observatoryName;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        public String getObservatoryName() { return observatoryName; }
+        public void setObservatoryName(String observatoryName) { this.observatoryName = observatoryName; }
+
+        public double getLatitude() { return latitude; }
+        public void setLatitude(double latitude) { this.latitude = latitude; }
+
+        public double getLongitude() { return longitude; }
+        public void setLongitude(double longitude) { this.longitude = longitude; }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "observatoryName='" + observatoryName + '\'' +
+                    ", latitude=" + latitude +
+                    ", longitude=" + longitude +
+                    '}';
+        }
+    }
+}
